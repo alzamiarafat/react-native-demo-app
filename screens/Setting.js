529f9c8b1a1a1c69b288cd4969d2188f78c1cd31
alzamiarafat <alzamiarafat00@gmail.com>
2022-02-16 13:15:12 +0600

    build under dev

--- screens/Setting.js
+++ screens/Setting.js
@@ -1,22 +1,72 @@
-import { View, Text, StyleSheet, StatusBar, Image, ScrollView, } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, StatusBar, ScrollView, } from 'react-native'
+import React, { useState } from 'react'
 import Header from '../componants/Header';
 import ProfileHeader from '../componants/ProfileHeader';
+import { Collapse, CollapseHeader, CollapseBody, AccordionList } from 'accordion-collapse-react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 export default function Setting() {
+    const [isExtand, setIsExtand] = useState(true);
+
+    const iconHandle = () => {
+        alert(165)
+        // if (isExtand) setIsExtand(false);
+        // else setIsExtand(true)
+    }
+
     return (
         <View style={styles.container}>
             <Header></Header>
             <ProfileHeader />
+
             <ScrollView style={{ marginTop: 50 }}>
                 <View style={{ backgroundColor: 'white', padding: 10, marginBottom: 50, }}>
-                    <Text style={{ padding: 15, borderBottomColor: 'lightgray', borderStyle: 'solid', borderBottomWidth: 1 }}>fdsf</Text>
-                    <Text style={{ padding: 15, borderBottomColor: 'lightgray', borderStyle: 'solid', borderBottomWidth: 1 }}>fdsf</Text>
-                    <Text style={{ padding: 15, borderBottomColor: 'lightgray', borderStyle: 'solid', borderBottomWidth: 1 }}>fdsf</Text>
+                    <Collapse style={{ padding: 15, paddingBottom: 0 }}>
+                        <CollapseHeader>
+                            <View style={{ flexDirection: 'row' }} onPress={iconHandle}>
+                                <Text style={{ flex: 8, fontSize: 17 }}>Profile Setting</Text>
+                                <Text style={{ flex: 1, fontSize: 17 }}>
+                                    <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+                                </Text>
+                            </View>
+                        </CollapseHeader>
+                        <CollapseBody>
+                            <Text>Ta daa!</Text>
+                        </CollapseBody>
+                    </Collapse>
+                    <View style={{ borderBottomColor: 'lightgray', borderBottomWidth: 1, borderStyle: 'solid', margin: 10, marginBottom: 0 }}></View>
+
+                    <Collapse style={{ padding: 15, paddingBottom: 0 }}>
+                        <CollapseHeader>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={{ flex: 8, fontSize: 17 }}>Account Setting</Text>
+                                <Text style={{ flex: 1, fontSize: 17 }}>
+                                    <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+                                </Text>
+                            </View>
+                        </CollapseHeader>
+                        <CollapseBody>
+                            <Text>Ta daa!</Text>
+                        </CollapseBody>
+                    </Collapse>
+                    <View style={{ borderBottomColor: 'lightgray', borderBottomWidth: 1, borderStyle: 'solid', margin: 10, marginBottom: 0 }}></View>
+
+                    <Collapse style={{ padding: 15, paddingBottom: 0 }}>
+                        <CollapseHeader>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={{ flex: 8, fontSize: 17 }}>Privacy Setting</Text>
+                                <Text style={{ flex: 1, fontSize: 17 }}>
+                                    <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+                                </Text>
+                            </View>
+                        </CollapseHeader>
+                        <CollapseBody>
+                            <Text>Ta daa!</Text>
+                        </CollapseBody>
+                    </Collapse>
                 </View>
             </ScrollView>
-
         </View>
     )
 }
