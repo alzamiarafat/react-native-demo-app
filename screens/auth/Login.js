b3917d7e0d936a407194ee58d41c6347b5afa7ea
alzamiarafat <alzamiarafat00@gmail.com>
2022-03-06 15:46:20 +0600

    desgin responsive underdev

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,38 +1,69 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Button, Alert } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Button, Alert, Dimensions } from 'react-native'
 import React from 'react';
 import { Ionicons, MaterialIcons, Entypo, FontAwesome } from '@expo/vector-icons';
 
+const { height, width } = Dimensions.get('window');
+
 export default function Login({ navigation }) {
     return (
-        <View style={styles.container}>
-            <View style={{ alignItems: 'center', marginTop: 10 }}>
-                <Image source={{ uri: 'https://pathwayport.com/saasland/images/login@4x.png' }} style={{ width: 350, height: 300 }} resizeMode='contain'></Image>
+        <View style={{ flex: 1, backgroundColor: 'red' }}>
+            <View style={{ backgroundColor: 'blue', marginTop: height - 800, alignItems: 'center' }}>
+                <Image source={{ uri: 'https://pathwayport.com/saasland/images/login@4x.png' }} style={{ width: width - 30, height: height - 500, backgroundColor: "green" }} resizeMode='contain'></Image>
             </View>
-            <Text style={styles.title}>Login</Text>
-            <View style={styles.body}>
-                <TextInput style={styles.input} placeholder="Email/Username" />
-                <TextInput style={[styles.input, styles.passwordInput]} secureTextEntry={true} placeholder="Password" />
-                <Ionicons name="eye" size={25} style={styles.iconStyle} color="#999" />
-                <Ionicons name="eye-off" size={25} style={styles.iconStyle} color="#999" />
-                <Text style={{ color: 'darkgray', fontSize: 17, textAlign: 'right', marginBottom: 10 }}>Forgot Password?</Text>
-                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                    <Text style={styles.btn}>Log In</Text>
-                </TouchableOpacity>
-                <Text style={{ color: 'black', textAlign: 'center', fontSize: 18, marginTop: 15 }}>Create an Account?</Text>
-                <Text style={{ color: 'gray', fontSize: 20, textAlign: 'center', marginTop: 15 }}>Or</Text>
-                <View style={styles.socialIcons}>
-                    <Text style={{ paddingHorizontal: 10 }}>
-                        <FontAwesome name="google-plus-circle" size={53} color="crimson" />
-                    </Text>
-                    <Text style={{ paddingHorizontal: 10 }}>
-                        <Entypo name="twitter-with-circle" size={50} color="deepskyblue" />
-                    </Text>
-                    <Text style={{ paddingHorizontal: 10 }}>
-                        <MaterialIcons name="facebook" size={50} color="royalblue" />
-                    </Text>
+            <View style={{ backgroundColor: 'yellow', alignItems: 'center' }}>
+                <View style={{ backgroundColor: 'skyblue', width: width - 30 }}>
+                    <Text style={styles.title}>Login</Text>
+                    <TextInput style={styles.input} placeholder="Email/Username" />
+                    {/* <View style={[styles.input, styles.passwordInput]}> */}
+                    <View style={{
+                        fontSize: 18,
+                        color: 'black',
+                        borderColor: 'lightgray',
+                        borderStyle: 'solid',
+                        borderWidth: 1,
+                        padding: 13,
+                        borderRadius: 5,
+                        marginVertical: 10,
+                    }}>
+                        <TextInput secureTextEntry={true} placeholder="Password" />
+                        <View style={{ backgroundColor: 'gray', alignItems: 'flex-end' }}>
+                            <Ionicons name="eye" size={25} style={styles.iconStyle} color="#999" />
+                            <Ionicons name="eye-off" size={25} style={styles.iconStyle} color="#999" />
+                        </View>
+
+                    </View>
                 </View>
             </View>
         </View>
+        // <View style={styles.container}>
+        //     <View style={{ alignItems: 'center', marginTop: 10 }}>
+        //         <Image source={{ uri: 'https://pathwayport.com/saasland/images/login@4x.png' }} style={{ width: 350, height: 300 }} resizeMode='contain'></Image>
+        //     </View>
+        //     <Text style={styles.title}>Login</Text>
+        //     <View style={styles.body}>
+        //         <TextInput style={styles.input} placeholder="Email/Username" />
+        //         <TextInput style={[styles.input, styles.passwordInput]} secureTextEntry={true} placeholder="Password" />
+        //         <Ionicons name="eye" size={25} style={styles.iconStyle} color="#999" />
+        //         <Ionicons name="eye-off" size={25} style={styles.iconStyle} color="#999" />
+        //         <Text style={{ color: 'darkgray', fontSize: 17, textAlign: 'right', marginBottom: 10 }}>Forgot Password?</Text>
+        //         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        //             <Text style={styles.btn}>Log In</Text>
+        //         </TouchableOpacity>
+        //         <Text style={{ color: 'black', textAlign: 'center', fontSize: 18, marginTop: 15 }}>Create an Account?</Text>
+        //         <Text style={{ color: 'gray', fontSize: 20, textAlign: 'center', marginTop: 15 }}>Or</Text>
+        //         <View style={styles.socialIcons}>
+        //             <Text style={{ paddingHorizontal: 10 }}>
+        //                 <FontAwesome name="google-plus-circle" size={53} color="crimson" />
+        //             </Text>
+        //             <Text style={{ paddingHorizontal: 10 }}>
+        //                 <Entypo name="twitter-with-circle" size={50} color="deepskyblue" />
+        //             </Text>
+        //             <Text style={{ paddingHorizontal: 10 }}>
+        //                 <MaterialIcons name="facebook" size={50} color="royalblue" />
+        //             </Text>
+        //         </View>
+        //     </View>
+        // </View>
     )
 }
 
@@ -50,7 +81,7 @@ const styles = StyleSheet.create({
         color: 'black',
         fontWeight: 'bold',
         textAlign: 'center',
-        marginBottom: 10,
+        marginBottom: height - 810,
     },
     input: {
         fontSize: 18,
@@ -78,8 +109,10 @@ const styles = StyleSheet.create({
     },
     iconStyle: {
         position: 'absolute',
-        top: '26%',
-        left: '90%'
+        backgroundColor: 'gray'
+        // top: '26%',
+        // left: '90%'
+        // justifyContent: 'center'
     },
     socialIcons: {
         flexDirection: 'row',
